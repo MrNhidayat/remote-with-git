68f087ad7499c796dde2766c076a6bb6b83854b3
Raihan <grandrx041@gmail.com>
2022-04-16 20:43:49 +0700

    menambahkan file

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+$nama = ["muhamad","raihan","nurhidayat"]
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>halo nama saya <?= $nama[0]?></h1>
+</body>
+</html>
